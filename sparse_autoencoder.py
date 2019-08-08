585fe994fa456eb742a41b9078b826ca2035aea5
kodamanbou <kodamanbou0424@gmail.com>
2019-08-08 16:44:27 +0900

    Add Autoencoder.

--- sparse_autoencoder.py
+++ sparse_autoencoder.py
@@ -63,7 +63,7 @@ def kl_divergence(p, q):
 
 
 X = tf.placeholder(tf.float32, [None, image_size, image_size, 3])
-is_training = tf.placeholder_with_default(False, shape=(), name='training')
+is_training = tf.placeholder(tf.bool, name='is_training')
 hidden1 = tf.layers.conv2d(X, kernel_size=5, filters=64, strides=2, padding='same', activation=tf.nn.leaky_relu)
 
 hidden2 = tf.layers.conv2d(hidden1, kernel_size=5, filters=128, strides=2, padding='same')
@@ -76,39 +76,28 @@ hidden4 = tf.layers.conv2d(hidden3, kernel_size=5, filters=512, strides=2, paddi
 hidden4 = tf.nn.leaky_relu(tf.layers.batch_normalization(hidden4, training=is_training))
 
 hidden5 = tf.layers.flatten(hidden4)
-hidden5 = tf.layers.dense(hidden5, n_hidden5_units)
+hidden5 = tf.layers.dense(hidden5, n_hidden5_units, activation=tf.nn.sigmoid)
 
-hidden6_mean = tf.layers.dense(hidden5, n_hidden5_units)
-hidden6_sigma = tf.layers.dense(hidden5, n_hidden5_units)
-noise = tf.random_normal(tf.shape(hidden6_sigma), dtype=tf.float32)
-hidden6 = hidden6_mean + hidden6_sigma * noise
-hidden6 = tf.nn.sigmoid(hidden6)
+hidden6 = tf.reshape(hidden5, [-1, 4, 4, 512])
+hidden6 = tf.nn.relu(tf.layers.batch_normalization(hidden6, training=is_training))
 
-hidden7 = tf.layers.dense(hidden6, n_hidden5_units)
-hidden7 = tf.reshape(hidden7, [-1, 4, 4, 512])
+hidden7 = tf.layers.conv2d_transpose(hidden6, kernel_size=5, filters=256, strides=2, padding='same')
 hidden7 = tf.nn.relu(tf.layers.batch_normalization(hidden7, training=is_training))
 
-hidden8 = tf.layers.conv2d_transpose(hidden7, kernel_size=5, filters=256, strides=2, padding='same')
+hidden8 = tf.layers.conv2d_transpose(hidden7, kernel_size=5, filters=128, strides=2, padding='same')
 hidden8 = tf.nn.relu(tf.layers.batch_normalization(hidden8, training=is_training))
 
-hidden9 = tf.layers.conv2d_transpose(hidden8, kernel_size=5, filters=128, strides=2, padding='same')
+hidden9 = tf.layers.conv2d_transpose(hidden8, kernel_size=5, filters=64, strides=2, padding='same')
 hidden9 = tf.nn.relu(tf.layers.batch_normalization(hidden9, training=is_training))
 
-hidden10 = tf.layers.conv2d_transpose(hidden9, kernel_size=5, filters=64, strides=2, padding='same')
-hidden10 = tf.nn.relu(tf.layers.batch_normalization(hidden10, training=is_training))
-
-logits = tf.layers.conv2d_transpose(hidden10, kernel_size=5, filters=3, strides=2, padding='same')
+logits = tf.layers.conv2d_transpose(hidden9, kernel_size=5, filters=3, strides=2, padding='same')
 outputs = tf.nn.sigmoid(logits)
 
+hidden5_mean = tf.reduce_mean(hidden5, axis=0)
+sparsity_loss = tf.reduce_sum(kl_divergence(sparsity_target, hidden5_mean))
 xentropy = tf.nn.sigmoid_cross_entropy_with_logits(labels=X, logits=logits)
 reconstruction_loss = tf.reduce_sum(xentropy)
-eps = 1e-10
-latent_loss = 0.5 * tf.reduce_sum(
-    tf.square(hidden6_sigma) + tf.square(hidden6_mean) - 1 - tf.log(eps + tf.square(hidden6_sigma))
-)
-sparsity_loss = tf.reduce_sum(kl_divergence(sparsity_target, tf.reduce_mean(hidden6, axis=0)))
-loss = reconstruction_loss + latent_loss + sparsity_weight * sparsity_loss
-
+loss = reconstruction_loss + sparsity_weight * sparsity_loss
 optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
 training_op = optimizer.minimize(loss)
 
@@ -120,12 +109,6 @@ config.gpu_options.allow_growth = True
 config.allow_soft_placement = True
 
 with tf.Session(config=config) as sess:
-    def gen_dogs(bs):
-        codings_rnd = np.random.normal(size=[bs, n_hidden5_units])
-        outputs_val = outputs.eval(feed_dict={hidden6: codings_rnd, is_training: False})
-        return outputs_val
-
-
     print('Training start.')
     prepro()
     init.run()
@@ -138,15 +121,17 @@ with tf.Session(config=config) as sess:
                 for img in all_images[offset:offset + batch_size]
             ])
             sess.run(training_op, feed_dict={X: X_batch, is_training: True})
-            loss_val, reconstruction_loss_val, latent_loss_val, sparsity_loss_val = sess.run(
-                [loss, reconstruction_loss, latent_loss, sparsity_loss],
-                feed_dict={X: X_batch, is_training: True})
+            loss_val, reconstruction_loss_val, sparsity_loss_val = sess.run([loss, reconstruction_loss, sparsity_loss],
+                                                                            feed_dict={X: X_batch, is_training: True})
             print("\r{}".format(epoch), "Train total loss:", loss_val, "\tReconstruction loss:",
-                  reconstruction_loss_val, "\tLatent loss:", latent_loss_val, "\tSparsity loss:", sparsity_loss_val)
+                  reconstruction_loss_val, "\tSparsity loss:", sparsity_loss_val)
 
         # plot.
-        sample_rnd = np.random.normal(size=[5, n_hidden5_units])
-        sample_outputs = outputs.eval(feed_dict={hidden6: sample_rnd, is_training: False})
+        X_test = np.array([
+            read_image(img, image_size, image_size)
+            for img in all_images[:5]
+        ])
+        sample_outputs = outputs.eval(feed_dict={X: X_test, is_training: False})
         plt.figure(figsize=(15, 3))
         for i, sample in enumerate(sample_outputs):
             plt.subplot(1, 5, i + 1)
@@ -154,23 +139,5 @@ with tf.Session(config=config) as sess:
             plt.axis('off')
             plt.imshow(sample)
         plt.show()
-
-    # Generate.
-    n_batches = n_generates // batch_size
-    n_last_batch_size = n_generates % batch_size
-    z = zipfile.PyZipFile('images.zip', mode='w')
-    for i in range(n_batches):
-        for j, img in enumerate(gen_dogs(batch_size)):
-            f = f'sample_{i}_{j}.png'
-            imsave(f, img)
-            z.write(f)
-            os.remove(f)
-
-    for k, img in enumerate(gen_dogs(n_last_batch_size)):
-        f = f'sample_last_{k}.png'
-        imsave(f, img)
-        z.write(f)
-        os.remove(f)
-    z.close()
 
 print('Training end.')
